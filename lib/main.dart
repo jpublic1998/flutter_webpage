020c35636c73a55185a4921a4a262c45e6403ce2
jpublic1998 <jijithshaji@outlook.com>
2021-04-24 19:00:33 +0530

    Revert "Revert "sweven""
    This reverts commit c7dff26be9467880e4c073f931c38cb8311f4c2a.

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Sweven Montessori',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(primarySwatch: Colors.blue, fontFamily: "Montserrat"),
       home: MyHomePage(),
